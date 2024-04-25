8c5530f8bafa5dd3bbb262bb9690c03cef2dea8b
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2024-04-25 20:45:18 -0300

    Update script.js
    Update calorie counter

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ function isInvalidInput(str) {
     return str.match(regex);
 }
 function addEntry() {
-    const targetId = "#" + entryDropdown.value;
-    const targetInputContainer = document.querySelector(targetId + " .input-container");
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>`;
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
+addEntryButton.addEventListener('click', addEntry);
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+    for(const item of list){
+        const currVal = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+        if (invalidInputMatch) {
+            
+        }
+    }
 }
